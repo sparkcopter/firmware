3c4916747827cd9807433540508d6a14e05c0715
James Smith <james@loopj.com>
2015-03-28 21:17:04 -0700

    MPU6050 library improvements

--- src/MPU6050.cpp
+++ src/MPU6050.cpp
@@ -14,15 +14,61 @@ void MPU6050::init() {
 
     // Wake up the MPU-6050 for communication
     Wire.beginTransmission(this->address);
-    Wire.write(MPU6050_PWR_MGMT_1);
+    Wire.write(MPU6050_RA_PWR_MGMT_1);
     Wire.write(0);
     Wire.endTransmission(true);
+
+    // Set up default gyro and accelerometers ranges
+    setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+    setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
 }
 
-void MPU6050::getMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz) {
+void MPU6050::setFullScaleGyroRange(uint8_t range) {
+    // Set the gyro range on the chip
+    writeBits(MPU6050_RA_GYRO_CONFIG, MPU6050_GCONFIG_FS_SEL_BIT, MPU6050_GCONFIG_FS_SEL_LENGTH, range);
+
+    // Calculate and save the gyro scaling factor
+    switch(range) {
+      case MPU6050_GYRO_FS_250:
+            this->gyroRes = 250.0/32768.0;
+            break;
+      case MPU6050_GYRO_FS_500:
+            this->gyroRes = 500.0/32768.0;
+            break;
+      case MPU6050_GYRO_FS_1000:
+            this->gyroRes = 1000.0/32768.0;
+            break;
+      case MPU6050_GYRO_FS_2000:
+            this->gyroRes = 2000.0/32768.0;
+            break;
+    }
+}
+
+void MPU6050::setFullScaleAccelRange(uint8_t range) {
+    // Set the accelerometer range on the chip
+    writeBits(MPU6050_RA_ACCEL_CONFIG, MPU6050_ACONFIG_AFS_SEL_BIT, MPU6050_ACONFIG_AFS_SEL_LENGTH, range);
+
+    // Calculate and save the accelerometer scaling factor
+    switch(range) {
+      case MPU6050_ACCEL_FS_2:
+            this->accelRes = 2.0/32768.0;
+            break;
+      case MPU6050_ACCEL_FS_4:
+            this->accelRes = 4.0/32768.0;
+            break;
+      case MPU6050_ACCEL_FS_8:
+            this->accelRes = 8.0/32768.0;
+            break;
+      case MPU6050_ACCEL_FS_16:
+            this->accelRes = 16.0/32768.0;
+            break;
+    }
+}
+
+void MPU6050::getRawMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz) {
     // Read 14 bytes of data, starting at the first byte of accelerometer data
     // Sensor data is 16-bit, so we have to read 2 bytes for each sensor
-    this->readBytes(MPU6050_ACCEL_XOUT_H, 14, buffer);
+    readBytes(MPU6050_RA_ACCEL_XOUT_H, 14, buffer);
 
     // 3 axes of accelerometer data
     *ax = (((int16_t)buffer[0]) << 8) | buffer[1];
@@ -37,10 +83,10 @@ void MPU6050::getMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int1
     *gz = (((int16_t)buffer[12]) << 8) | buffer[13];
 }
 
-void MPU6050::getAcceleration(int16_t* x, int16_t* y, int16_t* z) {
+void MPU6050::getRawAcceleration(int16_t* x, int16_t* y, int16_t* z) {
     // Read 6 bytes of data, starting at the first byte of accelerometer data
     // Sensor data is 16-bit, so we have to read 2 bytes for each sensor
-    this->readBytes(MPU6050_ACCEL_XOUT_H, 6, buffer);
+    readBytes(MPU6050_RA_ACCEL_XOUT_H, 6, buffer);
 
     // 3 axes of accelerometer data
     *x = (((int16_t)buffer[0]) << 8) | buffer[1];
@@ -48,10 +94,10 @@ void MPU6050::getAcceleration(int16_t* x, int16_t* y, int16_t* z) {
     *z = (((int16_t)buffer[4]) << 8) | buffer[5];
 }
 
-void MPU6050::getRotation(int16_t* x, int16_t* y, int16_t* z) {
+void MPU6050::getRawRotation(int16_t* x, int16_t* y, int16_t* z) {
     // Read 6 bytes of data, starting at the first byte of gyroscope data
     // Sensor data is 16-bit, so we have to read 2 bytes for each sensor
-    this->readBytes(MPU6050_GYRO_XOUT_H, 6, buffer);
+    readBytes(MPU6050_RA_GYRO_XOUT_H, 6, buffer);
 
     // 3 axes of gyroscope data
     *x = (((int16_t)buffer[0]) << 8) | buffer[1];
@@ -59,25 +105,86 @@ void MPU6050::getRotation(int16_t* x, int16_t* y, int16_t* z) {
     *z = (((int16_t)buffer[4]) << 8) | buffer[5];
 }
 
-int16_t MPU6050::getTemperature() {
+int16_t MPU6050::getRawTemperature() {
     // Read 2 bytes of data, starting at the first byte of temperature data
     // Sensor data is 16-bit, so we have to read 2 bytes for each sensor
-    this->readBytes(MPU6050_TEMP_OUT_H, 2, buffer);
-    int16_t temp = (((int16_t)buffer[0]) << 8) | buffer[1];
-
-    // Adjust raw data into celcius
-    return temp/340.00+36.53;
+    readBytes(MPU6050_RA_TEMP_OUT_H, 2, buffer);
+    return (((int16_t)buffer[0]) << 8) | buffer[1];
 }
 
-void MPU6050::readBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
+void MPU6050::getMotion(float* ax, float* ay, float* az, float* gx, float* gy, float* gz) {
+    int16_t rax, ray, raz, rgx, rgy, rgz;
+    getRawMotion(&rax, &ray, &raz, &rgx, &rgy, &rgz);
+
+    *ax = (float)rax * accelRes;
+    *ay = (float)ray * accelRes;
+    *az = (float)raz * accelRes;
+
+    *gx = (float)rgx * gyroRes;
+    *gy = (float)rgy * gyroRes;
+    *gz = (float)rgz * gyroRes;
+}
+
+void MPU6050::getAcceleration(float* x, float* y, float* z) {
+
+}
+
+void MPU6050::getRotation(float* x, float* y, float* z) {
+
+}
+
+int16_t MPU6050::getTemperature() {
+    // Adjust raw data into celcius
+    return getRawTemperature()/340.00 + 36.53;
+}
+
+int8_t MPU6050::readBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
+    int8_t count = 0;
+
     // Request n bytes of register data from the i2c bus
     Wire.beginTransmission(this->address);
     Wire.write(regAddr);
     Wire.endTransmission(false);
+    Wire.beginTransmission(regAddr);
     Wire.requestFrom(this->address, length);
 
     // Read requested registers into a buffer
-    for(int8_t i = 0; i < length; i++) {
-        data[i] = Wire.read();
+    for(; Wire.available(); count++) {
+        data[count] = Wire.read();
+    }
+
+    return count;
+}
+
+int8_t MPU6050::readByte(uint8_t regAddr, uint8_t *data) {
+    return readBytes(regAddr, 1, data);
+}
+
+bool MPU6050::writeBytes(uint8_t regAddr, uint8_t length, uint8_t *data) {
+    uint8_t status = 0;
+
+    Wire.beginTransmission(this->address);
+    Wire.write(regAddr);
+    Wire.write(data, length);
+    status = Wire.endTransmission();
+
+    return status == 0;
+}
+
+bool MPU6050::writeByte(uint8_t regAddr, uint8_t data) {
+    return writeBytes(regAddr, 1, &data);
+}
+
+bool MPU6050::writeBits(uint8_t regAddr, uint8_t bitStart, uint8_t length, uint8_t data) {
+    uint8_t b;
+    if(readByte(regAddr, &b) != 0) {
+        uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+        data <<= (bitStart - length + 1); // shift data into correct position
+        data &= mask; // zero all non-important bits in data
+        b &= ~(mask); // zero all important bits in existing byte
+        b |= data; // combine data with existing byte
+        return writeByte(regAddr, b);
+    } else {
+        return false;
     }
 }
--- src/MPU6050.h
+++ src/MPU6050.h
@@ -1,35 +1,85 @@
 #include "application.h"
 
 #define MPU6050_DEFAULT_ADDRESS  0x68
-#define MPU6050_PWR_MGMT_1       0x6B
 
 // Register addresses
-#define MPU6050_ACCEL_XOUT_H     0x3B
-#define MPU6050_TEMP_OUT_H       0x41
-#define MPU6050_GYRO_XOUT_H      0x43
+#define MPU6050_RA_GYRO_CONFIG      0x1B
+#define MPU6050_RA_ACCEL_CONFIG     0x1C
+#define MPU6050_RA_ACCEL_XOUT_H     0x3B
+#define MPU6050_RA_TEMP_OUT_H       0x41
+#define MPU6050_RA_GYRO_XOUT_H      0x43
+#define MPU6050_RA_PWR_MGMT_1       0x6B
+
+// Config register bits
+#define MPU6050_GCONFIG_FS_SEL_BIT      4
+#define MPU6050_GCONFIG_FS_SEL_LENGTH   2
+#define MPU6050_ACONFIG_AFS_SEL_BIT     4
+#define MPU6050_ACONFIG_AFS_SEL_LENGTH  2
+
+// Full-scale gyroscope ranges
+#define MPU6050_GYRO_FS_250         0x00
+#define MPU6050_GYRO_FS_500         0x01
+#define MPU6050_GYRO_FS_1000        0x02
+#define MPU6050_GYRO_FS_2000        0x03
+
+// Full-scale accelerometer ranges
+#define MPU6050_ACCEL_FS_2          0x00
+#define MPU6050_ACCEL_FS_4          0x01
+#define MPU6050_ACCEL_FS_8          0x02
+#define MPU6050_ACCEL_FS_16         0x03
 
 class MPU6050 {
 public:
+    // Constructors
     MPU6050();
     MPU6050(uint8_t address);
 
     // Wake up the MPU-6050
     void init();
 
-    // Get current accelerometer and gryoscope readings
-    void getMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz);
+    // Calibrate the sensors
+    void calibrate(float *gyroBias, float *accelBias);
 
-    // Get current accelerometer readings
-    void getAcceleration(int16_t* x, int16_t* y, int16_t* z);
+    // Set full-scale gyroscope range
+    void setFullScaleGyroRange(uint8_t range);
 
-    // Get current gyroscope readings
-    void getRotation(int16_t* x, int16_t* y, int16_t* z);
+    // Set full-scale accelerometer range
+    void setFullScaleAccelRange(uint8_t range);
 
-    // Get current temperature
+    // Get raw accelerometer and gryoscope readings
+    void getRawMotion(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz);
+
+    // Get raw accelerometer readings
+    void getRawAcceleration(int16_t* x, int16_t* y, int16_t* z);
+
+    // Get raw gyroscope readings
+    void getRawRotation(int16_t* x, int16_t* y, int16_t* z);
+
+    // Get raw temperature reading
+    int16_t getRawTemperature();
+
+    // Get scaled motion readings (acceleration in Gs, rotation in degrees per second)
+    void getMotion(float* ax, float* ay, float* az, float* gx, float* gy, float* gz);
+
+    // Get scaled accelerometer readings (in Gs)
+    void getAcceleration(float* x, float* y, float* z);
+
+    // Get scaled gyroscope readings (in degrees per second)
+    void getRotation(float* x, float* y, float* z);
+
+    // Get scaled temperature reading
     int16_t getTemperature();
 
 private:
-    void readBytes(uint8_t regAddr, uint8_t length, uint8_t *data);
+    int8_t readBytes(uint8_t regAddr, uint8_t length, uint8_t *data);
+    int8_t readByte(uint8_t regAddr, uint8_t *data);
+
+    bool writeBytes(uint8_t regAddr, uint8_t length, uint8_t *data);
+    bool writeByte(uint8_t regAddr, uint8_t data);
+    bool writeBits(uint8_t regAddr, uint8_t start, uint8_t length, uint8_t data);
+
     uint8_t buffer[14];
     uint8_t address;
+    float gyroRes;
+    float accelRes;
 };
