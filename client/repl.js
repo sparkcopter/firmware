9cd2a5e7730bde663e052203083b1935e39cd101
James Smith <james@loopj.com>
2015-03-29 19:39:37 -0700

    Support changing ip to client

--- client/repl.js
+++ client/repl.js
@@ -1,3 +1,3 @@
 var arDrone = require('ar-drone');
-var client  = arDrone.createClient({ip: "192.168.1.111"});
+var client  = arDrone.createClient({ip: (process.env.IP || "192.168.1.111")});
 client.createRepl();
