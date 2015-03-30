c63f2ad1bf635f87d8c78bf4dd0e83951f430c92
James Smith <james@loopj.com>
2015-03-29 19:42:09 -0700

    Slightly better serial logger

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -4,6 +4,40 @@ void Logger::init() {
     Serial.begin(LOGGER_SERIAL_SPEED);
 }
 
+void Logger::error(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    Serial.print(LOGGER_COLOR_RED);
+    log(format, args);
+    Serial.print(LOGGER_COLOR_RESET);
+    va_end(args);
+}
+
+void Logger::warn(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    Serial.print(LOGGER_COLOR_YELLOW);
+    log(format, args);
+    Serial.print(LOGGER_COLOR_RESET);
+    va_end(args);
+}
+
+void Logger::debug(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    Serial.print(LOGGER_COLOR_GRAY);
+    log(format, args);
+    Serial.print(LOGGER_COLOR_RESET);
+    va_end(args);
+}
+
+void Logger::info(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    log(format, args);
+    va_end(args);
+}
+
 void Logger::log(const char* format, ...) {
     char buffer[LOGGER_BUFFER_LENGTH];
     va_list args;
--- src/Logger.h
+++ src/Logger.h
@@ -1,12 +1,29 @@
+#pragma once
+
 #include <stdarg.h>
+#include <spark_wiring_usbserial.h>
 
-#include "application.h"
+#define LOGGER_SERIAL_SPEED     115200
+#define LOGGER_BUFFER_LENGTH    255
 
-#define LOGGER_SERIAL_SPEED 115200
-#define LOGGER_BUFFER_LENGTH 255
+#define LOGGER_COLOR_RED        "\033[31m"
+#define LOGGER_COLOR_GREEN      "\033[32m"
+#define LOGGER_COLOR_YELLOW     "\033[33m"
+#define LOGGER_COLOR_BLUE       "\033[34m"
+#define LOGGER_COLOR_MAGENTA    "\033[35m"
+#define LOGGER_COLOR_CYAN       "\033[36m"
+#define LOGGER_COLOR_GRAY       "\033[37m"
+#define LOGGER_COLOR_RESET      "\033[0m"
 
 class Logger {
 public:
     static void init();
+
+    static void error(const char* format, ...);
+    static void warn(const char* format, ...);
+    static void info(const char* format, ...);
+    static void debug(const char* format, ...);
+
+private:
     static void log(const char* format, ...);
 };
