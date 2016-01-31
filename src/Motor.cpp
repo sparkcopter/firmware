7a1fe1aec19019bef6491232a7794decfcfbce16
James Smith <james@loopj.com>
2016-01-31 01:42:05 -0800

    Re-add removed files

---
+++ src/Motor.cpp
@@ -0,0 +1,15 @@
+#include "Motor.h"
+#include "application.h"
+
+Motor::Motor(uint16_t pin) {
+    this->pin = pin;
+    pinMode(pin, OUTPUT);
+}
+
+void Motor::setSpeed(uint8_t speed) {
+    analogWrite(this->pin, speed);
+}
+
+void Motor::stop() {
+    this->setSpeed(0);
+}
---
+++ src/Motor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stdint.h>
+
+class Motor {
+public:
+    // Initialize a motor on the given pin
+    Motor(uint16_t pin);
+
+    // Set the speed of the motor (0 = off, 255 = full speed)
+    void setSpeed(uint8_t speed);
+
+    // Stop the motor
+    void stop();
+
+private:
+    uint16_t pin;
+};
